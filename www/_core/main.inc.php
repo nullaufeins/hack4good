a7f5733f53b0699c4e403b6bf4e457274a546f6c
Sören Kirchner <mail@soeren-kirchner.dev>
2023-09-28 19:55:20 +0200

    [ADD] Add auto BASE_URL

--- www/_core/main.inc.php
+++ www/_core/main.inc.php
@@ -1,7 +1,14 @@
 <?php
 
+$baseUrls = array(
+    "staging.hack-for-good.de" => "https://staging.hack-for-good.de",
+    "www.hack-for-good.de-localhost" => "https://www.hack-for-good.de-localhost",
+    "localhost" => "http://localhost:8888"
+    );
+
+print($_SERVER['SERVER_NAME']);
+
 define("BASE_PATH", __DIR__);
-// define("BASE_URL", "https://www.hack-for-good.de-localhost");
-define("BASE_URL", "https://staging.hack-for-good.de");
+define("BASE_URL", $baseUrls[$_SERVER['SERVER_NAME']] ?: "https://staging.hack-for-good.de");
 
 require_once "defaultpage.inc.php";
