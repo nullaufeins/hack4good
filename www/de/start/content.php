b0b39f19a606f33fca55c6e672b463cc65f4702f
Norma Driske <hello@normade.dev>
2023-10-05 00:38:33 +0200

    Clean start page

--- www/de/start/content.php
+++ www/de/start/content.php
@@ -5,20 +5,17 @@
         <a href="" target="_blank" rel=“noopener noreferrer“>
             <h3 class="hero">tbd, Leipzig</h3>
         </a>
-        <a href="https://twitter.com/nullaufeins_ev" target="_blank" rel=“noopener noreferrer“
-            style="margin-right:1rem;">
-            <span class="icon icon-insta"></span>
-            <a href="https://www.instagram.com/nullaufeins_ev" target="_blank" rel=“noopener noreferrer“>
-                <span class="icon icon-twitter"></span></a>
-            <p>
-                Viele Organisationen, Vereine und Einzelpersonen unserer Region sind unermüdlich, oft ehrenamtlich, für gemeinnützige Projekte tätig, 
-                engagieren sich im sozialen Bereich oder setzen sich für Umwelt- und Nachhaltigkeitsanliegen ein. Wir sind der Meinung, 
-                dass sie nicht nur geben, sondern von anderen Teilen der Gesellschaft auch etwas zurückbekommen sollten.
-            </p>
-            <p>
-                Aus diesem Grund veranstalten wir ein gemeinsames Hackathon-Wochenende, um solche Organisationen beim Finden von digitalen und kreativen Lösungen für Probleme 
-                in ihrer täglichen Arbeit zu unterstützen.
-            </p>
+        <p>
+            Viele Organisationen, Vereine und Einzelpersonen unserer Region sind unermüdlich, oft ehrenamtlich, für gemeinnützige Projekte tätig, 
+            engagieren sich im sozialen Bereich oder setzen sich für Umwelt- und Nachhaltigkeitsanliegen ein. Wir sind der Meinung, 
+            dass sie nicht nur geben, sondern von anderen Teilen der Gesellschaft auch etwas zurückbekommen sollten.
+        </p>
+        <p>
+            Aus diesem Grund veranstalten wir ein gemeinsames Hackathon-Wochenende, um Organisationen beim Finden von digitalen und kreativen Lösungen für Probleme 
+            in ihrer täglichen Arbeit zu unterstützen.
+        </p>
+        <p>Ihr braucht Unterstützung? <a href="<?= BASE_URL ?>/de/ngos">Jetzt anmelden!</a> Wir freuen uns darauf, euch kennenzulernen!</p>
+        <p>Wenn ihr das Event unterstützen möchtet, bitte kontaktiert uns! Mehr Informationen <a href="<?= BASE_URL ?>/de/supporters">hier.</a></p>
     </div>
     <div class="graphic">
         <img src="<?= BASE_URL ?>/assets/logos/key-visual.svg" alt="Hackolaus" />
--- www/en/start/content.php
+++ www/en/start/content.php
@@ -5,20 +5,17 @@
         <a href="" target="_blank" rel=“noopener noreferrer“>
             <h3 class="hero">tbd, Leipzig</h3>
         </a>
-        <a href="https://twitter.com/nullaufeins_ev" target="_blank" rel=“noopener noreferrer“
-            style="margin-right:1rem;">
-            <span class="icon icon-insta"></span>
-            <a href="https://www.instagram.com/nullaufeins_ev" target="_blank" rel=“noopener noreferrer“>
-                <span class="icon icon-twitter"></span></a>
-            <p>
-                Many NGOs, associations and individuals work hard to give to social, environmental or other
-                sustainability causes. We believe that they should not only give but also receive, that society should
-                give back to them.
-            </p>
-            <p>
-                This is why we are collaborating on a hackathon to support these organizations with digial and creative solutions to help
-                them focus more on delivering impact.
-            </p>
+        <p>
+            Many NGOs, associations and individuals work hard to give to social, environmental or other
+            sustainability causes. We believe that they should not only give but also receive, that society should
+            give back to them.
+        </p>
+        <p>
+            This is why we are collaborating on a hackathon to support these organizations with digial and creative solutions to help
+            them focus more on delivering impact.
+        </p>
+        <p><a href="<?= BASE_URL ?>/en/ngos">Call for Projects</a> - Apply with your project now!</p>
+        <p>If you want to support this event, please get in touch! More information <a href="<?= BASE_URL ?>/en/supporters">here.</a></p>
     </div>
     <div class="graphic">
         <img src="<?= BASE_URL ?>/assets/logos/key-visual.svg" alt="Hackolaus" />
