e4b5d1718df83d24819dbb58da9154da66a79e99
Norma Driske <hello@normade.dev>
2023-10-05 00:03:49 +0200

    Update Community

--- www/de/menu.inc.php
+++ www/de/menu.inc.php
@@ -3,7 +3,7 @@
     <a href="<?= BASE_URL ?>/de/agenda" class="nav-item">Ablauf</a>
     <a href="<?= BASE_URL ?>/de/venue" class="nav-item">Ort</a>
     <a href="<?= BASE_URL ?>/de/ngos" class="nav-item">Vereine</a>
-    <a href="<?= BASE_URL ?>/de/community" class="nav-item">Gemeinschaft</a>
+    <a href="<?= BASE_URL ?>/de/community" class="nav-item">Community</a>
     <a href="<?= BASE_URL ?>/de/supporters" class="nav-item">Unterstützer</a>
     <a href="<?= BASE_URL ?>/de/faq" class="nav-item">FAQ</a>
 </nav>
